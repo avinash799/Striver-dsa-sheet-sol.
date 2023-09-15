4a437177857225ff50b2b20d10b4efc5614ef39a
AVINASH RAI <76801210+avinash799@users.noreply.github.com>
2023-09-15 17:00:16 +0530

    Add files via upload

---
+++ largest element in the array.cpp
@@ -0,0 +1,14 @@
+#1 Largest element in the array
+
+#include <bits/stdc++.h> 
+int largestElement(vector<int> &arr, int n)
+ {
+    int largest=arr[0];
+    for (int i = 0; i <arr.size(); i++) {
+        if(arr[i]>largest){
+            largest=arr[i];
+        }
+       
+    }
+     return largest;
+}
