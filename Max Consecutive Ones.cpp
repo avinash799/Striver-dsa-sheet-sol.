b8b0c1dc404a3b018a5784dc19511ba567fb7e4d
AVINASH RAI <76801210+avinash799@users.noreply.github.com>
2023-09-21 21:21:39 +0530

    Add files via upload

---
+++ Max Consecutive Ones.cpp
@@ -0,0 +1,21 @@
+//Max consecutive ones 
+
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+          int max_cnt = 0, cnt = 0;
+        for( int i=0;i<nums.size();i++){
+            if (nums[i]== 1)
+            {
+               cnt++;
+             }
+            else 
+            {
+                cnt = 0;
+            }
+            max_cnt=max(max_cnt,cnt);
+        }
+        return max_cnt;
+     
+    }
+};
